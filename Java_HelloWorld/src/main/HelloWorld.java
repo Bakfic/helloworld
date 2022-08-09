5884f707380bd4b3e3b0acbd2b0e6f81f7557f0c
Bakfic <nagybakfic@gmail.com>
2022-08-09 20:50:15 +0300

    Init project
    First commit

---
+++ Java_HelloWorld/src/main/HelloWorld.java
@@ -0,0 +1,12 @@
+package main;
+
+public class HelloWorld {
+	
+	public static void main(String[] args) {
+		
+		System.out.println("Hello World!");
+		
+		System.out.println("5+3 = " + (5+3));
+	}
+
+}
